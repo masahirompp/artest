599d2c8f8e8a6e12c9a17fae94fad3ae4a65be00
masahirompp <masahirompp@gmail.com>
2015-07-04 19:53:43 +0900

    refactor

--- src/pc/js/app.ts
+++ src/pc/js/app.ts
@@ -2,7 +2,7 @@
 
 import Marker = require('./Marker');
 
-var $video, $canvas, $context, detector, posit;
+var $video, $canvas, $context;
 var renderer;
 var scene3, scene4;
 var camera3, camera4;
@@ -44,36 +44,64 @@ function onLoad() {
       });
 
     // AR初期化
-    detector = new AR.Detector();
-    posit = new POS.Posit(modelSize, $canvas.width);
+    var detector = new AR.Detector();
+    var posit = new POS.Posit(modelSize, $canvas.width);
     createRenderers();
     createScenes();
 
-    // ブラウザ描画に合わせてtickを実行
-    requestAnimationFrame(tick);
+    // 描画開始
+    start(detector, posit);
   }
 };
 
-/* 描画処理 */
-var result = null;
-function tick() {
-  requestAnimationFrame(tick);
+function start(detector, posit) {
 
-  if ($video.readyState === $video.HAVE_ENOUGH_DATA) {
-    $context.drawImage($video, 0, 0, $canvas.width, $canvas.height);
-    var imageData = $context.getImageData(0, 0, $canvas.width, $canvas.height);
-    var markers = detector.detect(imageData);
-    drawCorners(markers);
-    updateScenes(markers);
+  /* 各反復の処理 */
+  var result = null;
+  function tick() {
+    requestAnimationFrame(tick);
 
-    if (markers.length === 2 && !result) {
-      result = judge(markers);
-      console.log(result);
+    if ($video.readyState === $video.HAVE_ENOUGH_DATA) {
+      $context.drawImage($video, 0, 0, $canvas.width, $canvas.height);
+      var imageData = $context.getImageData(0, 0, $canvas.width, $canvas.height);
+      var markers = detector.detect(imageData);
+      drawCorners(markers);
+      updateScenes(markers);
+
+      if (markers.length === 2 && !result) {
+        result = judge(markers);
+        console.log(result);
+      }
+
+      render();
+    }
+  };
+
+  function updateScenes(markers) {
+    var corners, corner, pose, i, j;
+
+    if (markers.length > 0) {
+      corners = markers[0].corners;
+
+      for (j = 0; j < corners.length; ++j) {
+        corner = corners[j];
+
+        corner.x = corner.x - ($canvas.width / 2);
+        corner.y = ($canvas.height / 2) - corner.y;
+      }
+
+      pose = posit.pose(corners);
+
+      updateObject(model, pose.bestRotation, pose.bestTranslation);
     }
 
-    render();
-  }
-};
+    texture.children[0].material.map.needsUpdate = true;
+  };
+
+  // ブラウザ描画に合わせてtickを実行
+  requestAnimationFrame(tick);
+}
+
 
 function drawCorners(markers) {
   var corners, corner, i, j;
@@ -98,6 +126,7 @@ function drawCorners(markers) {
     $context.strokeRect(corners[0].x - 2, corners[0].y - 2, 4, 4);
   }
 };
+
 function createRenderers() {
   renderer = new THREE.WebGLRenderer();
   renderer.setClearColor(0xffffff, 1);
@@ -112,12 +141,14 @@ function createRenderers() {
   camera4 = new THREE.PerspectiveCamera(40, $canvas.width / $canvas.height, 1, 1000);
   scene4.add(camera4);
 };
+
 function render() {
   renderer.autoClear = false;
   renderer.clear();
   renderer.render(scene3, camera3);
   renderer.render(scene4, camera4);
 };
+
 function createScenes() {
 
   texture = createTexture();
@@ -161,25 +192,25 @@ function createModel() {
   var object = new THREE.Object3D();
   var geometry = new THREE.SphereGeometry(1, 40, 40, Math.PI);
   var material = new THREE.MeshPhongMaterial({ color: 0xfff1cf });
-  var cube = new THREE.Mesh( geometry, material );
+  var cube = new THREE.Mesh(geometry, material);
 
-  var circleGeometry = new THREE.CylinderGeometry( 0.7, 0.7, 1,32 );
-  var circle = new THREE.Mesh( circleGeometry, material );
+  var circleGeometry = new THREE.CylinderGeometry(0.7, 0.7, 1, 32);
+  var circle = new THREE.Mesh(circleGeometry, material);
   circle.position.x = -1
   circle.rotation.x = 0
   circle.rotation.y = 0
   circle.rotation.z = 1.6
 
-  var hitosasiyubiGeometry = new THREE.CylinderGeometry( 0.2, 0.2, 2.3, 32 );
-  var hitosasiyubi = new THREE.Mesh( hitosasiyubiGeometry, material );
+  var hitosasiyubiGeometry = new THREE.CylinderGeometry(0.2, 0.2, 2.3, 32);
+  var hitosasiyubi = new THREE.Mesh(hitosasiyubiGeometry, material);
   hitosasiyubi.position.x = 1
   hitosasiyubi.position.y = 0
   hitosasiyubi.position.z = 0.6
   hitosasiyubi.rotation.y = -0.4
   hitosasiyubi.rotation.z = 1.6
 
-  var nakayubiGeometry = new THREE.CylinderGeometry( 0.2, 0.2, 3, 32 );
-  var nakayubi = new THREE.Mesh( nakayubiGeometry, material );
+  var nakayubiGeometry = new THREE.CylinderGeometry(0.2, 0.2, 3, 32);
+  var nakayubi = new THREE.Mesh(nakayubiGeometry, material);
   nakayubi.position.x = 1
   nakayubi.position.y = 0
   nakayubi.position.z = 0
@@ -191,26 +222,6 @@ function createModel() {
   object.add(nakayubi);
 
   return object;
-};
-function updateScenes(markers) {
-  var corners, corner, pose, i, j;
-
-  if (markers.length > 0) {
-      corners = markers[0].corners;
-
-      for (j = 0; j < corners.length; ++j) {
-        corner = corners[j];
-
-        corner.x = corner.x - ($canvas.width / 2);
-        corner.y = ($canvas.height / 2) - corner.y;
-      }
-
-      pose = posit.pose(corners);
-
-      updateObject(model, pose.bestRotation, pose.bestTranslation);
-  }
-
-  texture.children[0].material.map.needsUpdate = true;
 };
 
 function updateObject(object, rotation, translation) {
@@ -242,7 +253,10 @@ function updatePose(id, error, rotation, translation) {
   + " pitch: " + Math.round(-pitch * 180.0 / Math.PI)
   + " roll: " + Math.round(roll * 180.0 / Math.PI);
 };
+
 window.onload = onLoad;
+
+/* Janken */
 
 function judge(markers: Marker[]) {
   var tmp1 = markers[0].corners.reduce(addCornerX, 0); // marker1の座標
